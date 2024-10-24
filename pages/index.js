66e86f81a8d831cf8f1a5e7f10d13fed3ad8f878
mariful-nyx <mariful@heliosinsider.com>
2024-10-24 10:36:07 +0600

    fix translate issue

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,28 @@
 import { useTranslation } from 'react-i18next';
 import i18n from '../i18n';
-import { useEffect } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 
 
 export default function Home({locale}) {
   const { t } = useTranslation();
+  const [isLoaded, setIsLoaded] = useState(false)
 
   useEffect(() => {
-    i18n.changeLanguage(locale);
+    const loadLanguage = () => {
+      i18n.changeLanguage(locale);
+      setIsLoaded(true)
+    }
+    loadLanguage()
   }, [locale]);
 
+  if (!isLoaded){
+    return <div>Loading....</div>
+  }
+
   return (
-    <>
+    <Suspense fallback={<div>Loading..</div>}>
        <p>{t("BLOGNEWS")}</p>
-    </>
+    </Suspense>
   );
 };
 
